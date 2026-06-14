f1baa0bf6f602945c0c9a46e9a9c1dba805f83bc
zizouuaziiz-debug <zizouuaziiz@gmail.com>
2026-06-14 20:13:24 +0100

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes/index";
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/index");
 
 const app = express();
 
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
-export default app;
+module.exports = app;
